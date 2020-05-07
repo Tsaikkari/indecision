d4ff728093872b4fb045002f199f31129959ad86
Tsaikkari <trospe@gmx.de>
2020-05-07 09:36:13 +0200

    Putting components into their own files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import reactDOM from 'react-dom';
-
-const template = <p>testing 123</p>;
-reactDOM.render(template, document.getElementById('app'));
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+  
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
 
